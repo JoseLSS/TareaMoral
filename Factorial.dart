04247437b974bce8adccbfbb1bfa2e791f8ef99a
Jose Luis Sanchez <88795628+JoseLSS@users.noreply.github.com>
2021-08-24 18:41:17 -0500

    Add files via upload

---
+++ Factorial.dart
@@ -0,0 +1,27 @@
+void main() {
+  /*-----------------otra manera
+  int fac=1;
+  for(int i=1 ; i<=5 ; i++){
+
+    fac= fac * i;
+     
+  }
+  print("$fac");
+  -------------------------------*/
+
+  ejecutaOperacion(factorial);
+}
+
+void factorial (int fac, int num ){
+  
+  for(int i=1 ; i<=num ; i++){
+
+    fac= fac * i;
+  }
+  print(fac);
+}
+
+
+void ejecutaOperacion(Function mifuncion){
+  mifuncion(1,3);
+}
